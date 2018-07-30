efd4acea494ecd4a953e7959a19d876f337b4d3f
PetersAndrewR <39889250+PetersAndrewR@users.noreply.github.com>
2018-07-30 15:28:42 -0400

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,108 @@
+# Yokogawa DX100P Temp Reader and Recorder for PC
+# 14MAY2018 by Andrew Peters
+#indention is tabs
+import TempReader
+import multiprocessing
+import tkinter
+from tkinter import messagebox
+
+def statusDecoder(status):
+	#Determines what image to use when updating main window	
+		global image0, image1, image2, image3
+		if status == 'ready':
+			return image0
+		elif status == 'testing':
+			return image1
+		elif status == 'hot':
+			return image2
+		elif status == 'error':
+			return image3
+		else:
+			print("Problem with images")
+			return
+		
+def refresh():
+	#Updates the images on the main Window that show Probe Status
+	global parent_conn1, parent_conn2, parent_conn3, parent_conn4, parent_conn5, parent_conn6
+	if parent_conn1.poll():
+		status = parent_conn1.recv()
+		label0.config(image=statusDecoder(status))
+	if parent_conn2.poll():
+		status = parent_conn2.recv()
+		label1.config(image=statusDecoder(status))
+	if parent_conn3.poll():
+		status = parent_conn3.recv()
+		label2.config(image=statusDecoder(status))
+	if parent_conn4.poll():
+		status = parent_conn4.recv()
+		label3.config(image=statusDecoder(status))
+	if parent_conn5.poll():
+		status = parent_conn5.recv()
+		label4.config(image=statusDecoder(status))
+	if parent_conn6.poll():
+		status = parent_conn6.recv()
+		label5.config(image=statusDecoder(status))
+	print("REFRESHING!")
+	root.after(1000, refresh)
+
+if __name__ == '__main__':    #Prevents new process' from opening a new main window when a test is started
+	
+	#Duplex Pipe assignment, the parents stay with the main window, the child_conn is passed to the test process.
+	parent_conn1, child_conn1 = multiprocessing.Pipe()
+	parent_conn2, child_conn2 = multiprocessing.Pipe()
+	parent_conn3, child_conn3 = multiprocessing.Pipe()
+	parent_conn4, child_conn4 = multiprocessing.Pipe()
+	parent_conn5, child_conn5 = multiprocessing.Pipe()
+	parent_conn6, child_conn6 = multiprocessing.Pipe()
+
+	pipeArrayC = [child_conn1, child_conn2, child_conn3, child_conn4, child_conn5, child_conn6]
+	pipeArrayP = [parent_conn1, parent_conn2, parent_conn3, parent_conn4, parent_conn5, parent_conn6]
+
+#Main Window
+	root = tkinter.Tk()
+	root.geometry('310x425')
+	root.title("ThermoReader9001")
+	root.columnconfigure(0, minsize=150)
+
+	btn1 = tkinter.Button(root, text="Start Test", command=lambda: TempReader.StartTestClick(pipeArrayC))  #The lambda expression allows me to pass variables to the function when the button is clicked.
+	btn2 = tkinter.Button(root, text="Example Output")
+	btn2.grid(column=0, row=3)
+	btn1.grid(column=0, row=1)
+
+	Space1 = tkinter.Frame(root)
+	Space3 = tkinter.Frame(root)
+	Space5 = tkinter.Frame(root)
+	Space1.grid(column=0, row=0)
+	Space3.grid(column=0, row=2)
+	Space5.grid(column=0, row=4)
+
+	image0 = tkinter.PhotoImage(file="C://Users/USER/Documents/ready.png")
+	image1 = tkinter.PhotoImage(file="C://Users/USER/Documents/test_run.png")
+	image2 = tkinter.PhotoImage(file="C://Users/USER/Documents/temp_met.png")
+	image3 = tkinter.PhotoImage(file="C://Users/USER/Documents/probe_er.png")
+
+	label0 = tkinter.Label(image=image0)
+	label1 = tkinter.Label(image=image0)
+	label2 = tkinter.Label(image=image0)
+	label3 = tkinter.Label(image=image0)
+	label4 = tkinter.Label(image=image0)
+	label5 = tkinter.Label(image=image0)
+
+	label0.grid(column=1, row=0, pady=5)
+	label1.grid(column=1, row=1)
+	label2.grid(column=1, row=2)
+	label3.grid(column=1, row=3)
+	label4.grid(column=1, row=4)
+	label5.grid(column=1, row=5)
+	'''
+	messagebox.showinfo("Quick Guide",
+                    "TempReader will record the temperature for a specified probe every second as well as determining the WT, CT, and EXO.  "
+                    "Temp Reader will then save the resulting information to an appropriately named excel file.\n\nAn example is packaged with this program.  "
+                    "To view press the Example button on the main window.\n\nThings to Consider:\n\nIf you close the main menu, TempReader will cease "
+                    "all recording.  Leave the Main Window open until all tests are complete.\n\nThe program decides when the test is over and to stop "
+                    "recording when the temperature drops 10 degrees.  If you cancel a test early and a drop of 10 degrees or more does not occur TempReader"
+                    "will continue to record.  Simple remedy is to dip the probe in cold water.")
+	'''
+	refresh()
+	
+	root.mainloop()
---
+++ TempReader.py
@@ -0,0 +1,200 @@
+#indention is 4 spaces, not tabs
+import urllib.request
+import time
+import xlsxwriter
+import tkinter
+import multiprocessing
+
+#Create New Test Window
+def StartTestClick(pipeArray):
+    NewTestInfo = [' ', ' ', ' ', ' ']  # [ProdName, Lot#, Test#/Letter, Probe#]
+    ok = tkinter.IntVar()
+    radBut = tkinter.IntVar()
+
+    newTest = tkinter.Toplevel()
+    newTest.geometry('350x125')
+    newTest.title("New Test")
+    lbl1 = tkinter.Label(newTest, text="Product Name")
+    lbl2 = tkinter.Label(newTest, text="Lot #")
+    lbl3 = tkinter.Label(newTest, text="Test #/Letter")
+    lbl4 = tkinter.Label(newTest, text="Probe Number")
+    lbl1.grid(column=0, row=0, sticky='E')
+    lbl2.grid(column=0, row=1, sticky='E')
+    lbl3.grid(column=0, row=2, sticky='E')
+    lbl4.grid(column=0, row=3, sticky='E')
+
+    txt1 = tkinter.Entry(newTest, width=15)
+    txt2 = tkinter.Entry(newTest, width=15)
+    txt3 = tkinter.Entry(newTest, width=5)
+    txt1.grid(column=1, row=0)
+    txt2.grid(column=1, row=1)
+    txt3.grid(column=1, row=2, sticky='W')
+
+    rad1 = tkinter.Radiobutton(newTest, text='1', value=1, variable=radBut)
+    rad2 = tkinter.Radiobutton(newTest, text='2', value=2, variable=radBut)
+    rad3 = tkinter.Radiobutton(newTest, text='3', value=3, variable=radBut)
+    rad4 = tkinter.Radiobutton(newTest, text='4', value=4, variable=radBut)
+    rad5 = tkinter.Radiobutton(newTest, text='5', value=5, variable=radBut)
+    rad6 = tkinter.Radiobutton(newTest, text='6', value=6, variable=radBut)
+    rad1.grid(column=1, row=3, sticky='E')
+    rad2.grid(column=2, row=3)
+    rad3.grid(column=3, row=3)
+    rad4.grid(column=4, row=3)
+    rad5.grid(column=5, row=3)
+    rad6.grid(column=6, row=3)
+
+    txt1.focus()
+	
+    Space1 = tkinter.Frame(newTest)
+    Space1.grid(column=0, row=4)
+
+    def okClick():
+        ok.set(1)
+    def cancelClick():
+        ok.set(0)
+
+    but1 = tkinter.Button(newTest, text='OK', width=10, command=okClick)
+    but1.place(height=26, width=90, relx=0.15, rely=0.71)
+    but2 = tkinter.Button(newTest, text='Cancel', width=10, command=cancelClick)
+    but2.place(height=26, width=90, relx=0.50, rely=0.71)
+
+    newTest.focus()
+    txt1.focus()
+
+ #print(ok.get())
+
+    but1.wait_variable(ok)
+
+    #print(ok.get())
+
+    NewTestInfo[0] = txt1.get()
+    NewTestInfo[1] = txt2.get()
+    NewTestInfo[2] = txt3.get()
+    NewTestInfo[3] = radBut.get()
+
+    if(ok.get() == 1):
+        p = multiprocessing.Process(target=TestStart, args=(NewTestInfo, pipeArray[NewTestInfo[3]-1]))    # -1 since NewTestInfo is an array that starts at [0] and probe numbers start at 1
+        p.start()
+    if(ok.get() == 0):
+        print("User Canceled")
+
+    newTest.destroy()
+
+def TestStart(NewTestInfo, child_conn):
+    # Starting point for recording test data
+    fileName = fileNamer(NewTestInfo[0], NewTestInfo[1], NewTestInfo[2])
+    # initializes the excel workbook and sheet
+    workbook = xlsxwriter.Workbook(fileName)
+    worksheet = workbook.add_worksheet()
+
+    bold = workbook.add_format({'bold': True})
+
+    worksheet.write('A1', 'TIME', bold)
+    worksheet.write('B1', 'TEMP', bold)
+    worksheet.write('D1', 'PRODUCT', bold)
+    worksheet.write('E1', 'LOT', bold)
+    worksheet.write('D2', NewTestInfo[0])
+    worksheet.write('E2', NewTestInfo[1])
+    worksheet.write('D4', 'FINAL WT', bold)
+    worksheet.write('D5', 'FINAL RT', bold)
+    worksheet.write('D6', 'FINAL EXO', bold)
+    # After initializing the excel sheet, we start recording
+    finalTimes = [0.0, 0.0, 0]
+    child_conn.send('testing')
+    #child_conn.close()
+    finalTimes = timeKeeper(NewTestInfo[3], worksheet, child_conn)
+
+    for i in (0, 1):
+        finalTimes[i] = timeStandardizer(finalTimes[i])
+    # writes final times/temp on excel sheet
+    worksheet.write('E4', finalTimes[0], bold)
+    worksheet.write('E5', finalTimes[1], bold)
+    worksheet.write('E6', finalTimes[2], bold)
+
+    workbook.close()
+
+    child_conn.send('ready')
+    child_conn.close()
+
+    return
+
+def fileNamer(prodName, prodLot, testNum):
+    # assembles the string responsible for excel file name and location.
+    fileName = 'C://USERS/USER/Desktop/ProgTest/' + prodName + '_' + prodLot + '_' + testNum + '.xlsx'
+    return fileName
+
+def timeKeeper(pNumber, worksheet, child_conn):
+    # Counts Time for infoGrabber and Final Test Times
+    flag = '1'
+    count = 1
+    tempMax = int('0')
+    finalTimes = [0.0, 0.0, 0]  # [WT, RT, EXO]
+    startTime = time.time()
+
+    while (flag == '1'):
+        time.sleep(.9)  # stops the function for 0.9 seconds to ensure were not requesting temp more often then 1/sec
+        curTemp = infoGrabber(pNumber)
+        count = eXcelWriter(time.time() - startTime, curTemp, count, worksheet)  # writes current sec count since start and current temp, function returns count + 1 to ensure next excel cell is used
+        if (curTemp > tempMax + 100):
+            child_conn.send('error')
+            #child_conn.close()
+        if (curTemp > tempMax):  # updates max temp and time in an array
+            tempMax = curTemp
+            finalTimes[1] = time.time() - startTime
+            finalTimes[2] = tempMax
+        if (curTemp >= 105 and finalTimes[0] == 0.0):  # records WT
+            finalTimes[0] = time.time() - startTime
+            child_conn.send('hot')
+            #child_conn.close()
+            print("105F TIMES MET/RECORDED!")  # Only used during testing
+        if ((tempMax - 10) > curTemp and curTemp < tempMax):  # breaks the while loop to end the function
+            flag = '0'
+    return (finalTimes)
+
+def infoGrabber(pNumber):
+    # Requests current temperature information from Yokogawa DX100P and returns the requested probe temp
+    with urllib.request.urlopen('http://192.168.1.137/cgi-bin/ope/allch.cgi') as f:
+        raw_data = f.read()  # converts source information from yokogawa into a string
+    if (pNumber == 1):  # based on probe number, cuts all information except requested probe temp from the string
+        raw_data = raw_data[2269:-4035]
+        raw_data = int(raw_data.decode('utf-8'))
+        return raw_data
+    elif (pNumber == 2):
+        raw_data = raw_data[3056:-3248]
+        raw_data = int(raw_data.decode('utf-8'))
+        return raw_data
+    elif (pNumber == 3):
+        raw_data = raw_data[3842:-2461]
+        raw_data = int(raw_data.decode('utf-8'))
+        return raw_data
+    elif (pNumber == 4):
+        raw_data = raw_data[4629:-1674]
+        raw_data = int(raw_data.decode('utf-8'))
+        return raw_data
+    elif (pNumber == 5):
+        raw_data = raw_data[5416:-887]
+        raw_data = int(raw_data.decode('utf-8'))
+        return raw_data
+    elif (pNumber == 6):
+        raw_data = raw_data[6203:-100]
+        raw_data = int(raw_data.decode('utf-8'))
+        return raw_data
+
+def eXcelWriter(curTime, curTemp, count, worksheet):
+    # Writes read data from Yokogawa to excel sheet
+    worksheet.write(count, 0, curTime)
+    worksheet.write(count, 1, curTemp)
+    print("Writing to Excel File")  # Only used in testing
+    return count + 1
+
+def timeStandardizer(time):
+    # Converts from decimal minutes to min:sec
+    x = 0.0
+    time = (time - (time % 1))
+    x = time / 60
+    x = (x - (x % 1))
+    if (int(time - (x * 60))) < 10:
+        time = str(int(x)) + ':' + '0' + str(int(time - (x * 60)))	
+    else:
+        time = str(int(x)) + ':' + str(int(time - (x * 60)))
+    return time
